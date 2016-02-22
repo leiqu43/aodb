7494cc1d310dc5e938580f942c5f2533175f82cb
leiqu43 <leiqu43@gmail.com>
2016-02-22 08:57:59 -0500

    Update table.h

--- table.h
+++ table.h
@@ -35,19 +35,19 @@ namespace aodb {
 class PosixRandomAccessFile;
 
 //
-// 一个表是由一个(key,value)字典组成，其中一个表包括如下三部分：磁盘文件、磁盘索引，内存索引(map or sorted array)。
+// One table includes a dict of (key, value), they are file and index on harddrive, index in memory
 //
 class Table : boost::noncopyable {
 public:
     //
-    // 打开一个Table。
-    //  table_path : 表数据所保存的目录
-    //  filename : 表名称
-    //  max_table_size : 最大表的大小(key数量)
-    //  max_file_size : 最大数据文件大小
-    //  read_only : 是否只读
+    // Open a table
+    //  table_path 
+    //  filename 
+    //  max_table_size 
+    //  max_file_size
+    //  read_only
     //
-    //  如果成功，那么*table返回创建成功的表。如果失败，retval < 0
+    // 
     //
     static int Open(const std::string& table_path, 
                     const std::string& table_name,
@@ -57,7 +57,7 @@ public:
     ~Table();
 
     //
-    // 得到表名
+    // Talbe Name
     //
     std::string TableName() {
         return table_name_;
@@ -69,7 +69,7 @@ public:
     }
 
     //
-    // 是否已经写满了？
+    // Check is full?
     //
     bool CheckFull() {
         boost::mutex::scoped_lock table_put_lock(table_put_lock_);
@@ -77,17 +77,16 @@ public:
     }
 
     //
-    // 从表中获取数据
+    // Get data from table
     //
     int Get(const std::string& key, std::string* value);
 
     //
-    // 写入数据到表中
+    // write data into table
     //
     int Put(const std::string& key, const std::string& value);
 
 
-    // 启动后台处理线程
     void RunBgWorkThread();
 
 private:
@@ -98,21 +97,17 @@ private:
                    bool read_only);
 
     //
-    // 初始化db
+    // Init DB
     //
     int Initialize();
 
     //
-    // 更新内存索引
+    // Update index in memory
     //
     void UpdateIndexDict(const struct aodb_index& aodb_index);
 
     //
-    // 从索引中查询一个item
-    // retval :
-    //      <0  :   失败
-    //      0   :   没有找到
-    //      1  :   成功
+    // search an item from index
     //
     int GetIndex(const std::string& key, struct aodb_index* aodb_index);
 
@@ -122,59 +117,52 @@ private:
     void BgThread();
 
     //
-    // 标记为只读
+    // mark as read only
     //
     void MarkAsReadOnly();
 
-    // 保存只读索引
+    // save read-only index
     int SaveSortedIndex();
 
-    // 加载自读索引
+    // load index
     int LoadSortedIndex();
 
-    // 加载未排序索引数据
+    // load temporary index
     int LoadTmpIndex();
 
-    // 从dict中查找索引
+    // get index from dict
     int GetIndexFromDict(uint64_t key_sign, struct aodb_index* aodb_index);
 
-    // 从只读索引中查找
+    // get index from read-only index
     int GetIndexFromSortedIndex(uint64_t key_sign, struct aodb_index* aodb_index);
 
 private:
-    // 表路径&名称
     const std::string table_path_;
     const std::string table_name_;
 
-    // 索引文件路径
     std::string index_file_path_;
-    // 临时索引文件路劲
     std::string tmp_index_file_path_;
-    // 数据文件路径
     std::string data_file_path_;
 
-    // 索引文件
     PosixRandomAccessFile *tmp_index_file_writer_;
-    // 数据文件
     PosixRandomAccessFile *data_file_reader_writer_;
 
-    // aodb内存索引
+    // aodb index in memory
     std::map<uint64_t, struct aodb_index> index_dict_;
     boost::mutex index_dict_lock_;
 
     // read only sorted index
     std::vector<struct aodb_index> sorted_index_array_;
 
-    // 写数据锁，保证每个时刻只有一个用户写数据
+    // write lock
     boost::mutex table_put_lock_;
 
-    // 最大表的大小
     uint32_t max_table_size_;
 
-    // 是否是只读的
+    // read only or not
     bool read_only_;
 
-    // 是否正在排序中
+    // is it sorting?
     bool in_sorting_;
 
     boost::thread* bg_thread_;
